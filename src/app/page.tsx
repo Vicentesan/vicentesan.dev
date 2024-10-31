177596b146b6aa120d3a955014910587e8183b20
Vicentesan <vikom.sanchez@gmail.com>
2024-10-31 20:25:23 -0300

    chore: lint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,12 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center gap-4 p-24">
       <div className="flex flex-col items-center gap-2 text-center">
         <h1 className="text-4xl font-bold">Vicente Sanchez</h1>
-        <p className="text-muted-foreground text-lg">
+        <p className="text-lg text-muted-foreground">
           Obsessed with building products that help people
         </p>
       </div>
 
-      <p className="text-muted-foreground text-sm">under construction</p>
+      <p className="text-sm text-muted-foreground">under construction</p>
     </main>
   )
 }
