e8bd9a7d553e469d553454865f5a796e8f89db8a
Vicentesan <vikom.sanchez@gmail.com>
2024-11-07 21:56:43 -0300

    chore: lint

--- src/components/quote-button.tsx
+++ src/components/quote-button.tsx
@@ -63,7 +63,7 @@ export function QuoteButton({
   const { dictionary } = useLanguage()
 
   return (
-    <div className="xs:w-[498px] flex w-[238px] flex-col items-center justify-center gap-2">
+    <div className="flex w-[238px] flex-col items-center justify-center gap-2 xs:w-[498px]">
       <Button className="w-full" onClick={() => setIsQuoteModalOpen(true)}>
         <div
           className="group flex items-center justify-center gap-2 transition-colors duration-200"
